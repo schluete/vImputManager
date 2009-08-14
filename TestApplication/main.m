832d63826450eb2df46f9e5c91bb8fcdcfda6160
Axel Schlueter <axel@aroxis.de>
2009-08-14 03:41:18 +0200

    added an NSTextField to the text application

--- TestApplication/main.m
+++ TestApplication/main.m
@@ -1,14 +1,8 @@
-//
-//  main.m
-//  Testing
-//
 //  Created by Axel on 13.08.09.
-//  Copyright pqrs.de,2009. All rights reserved.
-//
+//  Copyright 2009 pqrs.de, All rights reserved.
 
 #import <Cocoa/Cocoa.h>
 
-int main(int argc, char *argv[])
-{
-    return NSApplicationMain(argc,  (const char **) argv);
+int main(int argc,char **argv) {
+  return NSApplicationMain(argc,(const char **)argv);
 }
