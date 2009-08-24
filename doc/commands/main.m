f51076197b83803a59a8687bdf1c4598bba020ae
Axel Schlueter <axel@aroxis.de>
2009-08-24 13:14:42 +0200

    remove testing stuff

--- doc/commands/main.m
+++ doc/commands/main.m
@@ -5,21 +5,18 @@ int main(int argc,const char **argv) {
   NSAutoreleasePool *pool=[[NSAutoreleasePool alloc] init];
 
   Commands *cmds=[[Commands alloc] init];
- 
-  [cmds processInput:'f' withControl:FALSE];
-  [cmds processInput:'c' withControl:FALSE];
-  [cmds processInput:'f' withControl:FALSE];
 
-#if 0
+  [cmds processInput:'3' withControl:FALSE];
+  [cmds processInput:'G' withControl:FALSE];      // goto third line
+
+  [cmds processInput:'0' withControl:FALSE];      // at the beginning of the line
+
   [cmds processInput:'1' withControl:FALSE];
   [cmds processInput:'0' withControl:FALSE];
-  [cmds processInput:'G' withControl:FALSE];
+  [cmds processInput:'l' withControl:FALSE];      // then move ten chars to the right
 
-  [cmds processInput:'l' withControl:FALSE];
-
-  [cmds processInput:'f' withControl:TRUE];
-  [cmds processInput:'b' withControl:TRUE];
-#endif
+//  [cmds processInput:'c' withControl:FALSE];
+//  [cmds processInput:'f' withControl:FALSE];
 
   [pool drain];
   return 0;
